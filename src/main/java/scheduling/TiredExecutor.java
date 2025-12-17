f235e5c6a2df2f86b723bc0f522c2831c46696fc
Doron Baras <barasdo@post.bgu.ac.il>
2025-12-17 14:22:15 +0200

    finish TiredExecutor but still need to check

--- src/main/java/scheduling/TiredExecutor.java
+++ src/main/java/scheduling/TiredExecutor.java
@@ -28,20 +28,103 @@ public class TiredExecutor {
     }
 
     public void submit(Runnable task) {
-        // TODO
+        if (task == null) {
+            throw new IllegalArgumentException("Task cannot be null");
+        }
 
+        TiredThread worker;
+        try {
+            worker = idleMinHeap.take();
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            return;
+        }
+
+        inFlight.incrementAndGet();
+
+        try {
+            Runnable wrapped = () -> {
+                try {
+                    task.run();
+                } finally {
+
+                    synchronized (TiredExecutor.this) {
+                        idleMinHeap.add(worker);
+
+                        if (inFlight.decrementAndGet() == 0) {
+                            TiredExecutor.this.notifyAll();
+                        }
+                    }
+                }
+            };
+
+            worker.newTask(wrapped);
+
+        } catch (IllegalStateException e) {
+
+            synchronized (this) {
+                inFlight.decrementAndGet();
+                idleMinHeap.add(worker);
+                notifyAll();
+            }
+            throw e;
+        }
     }
 
     public void submitAll(Iterable<Runnable> tasks) {
         // TODO: submit tasks one by one and wait until all finish
-    }
+        for (Runnable task : tasks) {
+            submit(task);
+        }
+        synchronized (this){
+            while (inFlight.get() > 0){
+                try {
+                    this.wait();
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+            }
+        }
+        }
+
+
+
 
     public void shutdown() throws InterruptedException {
         // TODO
+        synchronized (this){
+            while (inFlight.get() > 0){
+                try {
+                    this.wait();
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                    throw e;
+                }
+            }
+        }
+        for (TiredThread worker : workers){
+            worker.shutdown();
+            worker.join();
+        }
     }
 
     public synchronized String getWorkerReport() {
         // TODO: return readable statistics for each worker
-        return null;
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("============== WORKER REPORT ==============\n");
+
+        for (TiredThread w : workers) {
+            sb.append("Worker #").append(w.getWorkerId())
+                    .append(" | Busy: ").append(w.isBusy())
+                    .append(" | Fatigue: ").append(w.getFatigue())
+                    .append(" | Work Time: ").append(w.getTimeUsed() / 1_000_000.0).append(" ms")
+                    .append(" | Idle Time: ").append(w.getTimeIdle() / 1_000_000.0).append(" ms")
+                    .append("\n");
+        }
+
+        sb.append("==========================================\n");
+        return sb.toString();
     }
 }
+
